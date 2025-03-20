a3aa32fbddda8cab402201c5173c448937a7ec5f
KevinPladdet <91886057+KevinPladdet@users.noreply.github.com>
2025-03-20 09:48:46 +0100

    Pirate opacity fades and destroys itself when hit

--- Assets/Scripts/PirateScript.cs
+++ Assets/Scripts/PirateScript.cs
@@ -22,6 +22,28 @@ public class PirateScript : MonoBehaviour
             {
                 gm.LevelCompleted();
             }
+            StartCoroutine(FadePirate());
         }
+    }
+
+    private IEnumerator FadePirate()
+    {
+        yield return new WaitForSeconds(2f);
+
+        float duration = 1f;
+        float elapsedTime = 0f;
+
+        SpriteRenderer sr = this.GetComponent<SpriteRenderer>();
+
+        Color color = sr.color;
+        
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+            sr.color = color;
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
